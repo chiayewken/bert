cc7a2533a8b23624b0c9b70714d8a19f3f690ed4
Chia Yew Ken <chiayewken@gmail.com>
2019-08-14 17:06:11 +0800

    sts

--- run_regressor.py
+++ run_regressor.py
@@ -396,7 +396,7 @@ class STSProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return []
+    return ["score"]
 
   def _create_examples(self, lines, set_type):
       """Creates examples for the training and dev sets."""
@@ -636,41 +636,53 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
   hidden_size = output_layer.shape[-1].value
 
-  # output_weights = tf.get_variable(
-  #     "output_weights", [num_labels, hidden_size],
-  #     initializer=tf.truncated_normal_initializer(stddev=0.02))
+  output_weights = tf.get_variable(
+      "output_weights", [num_labels, hidden_size],
+      initializer=tf.truncated_normal_initializer(stddev=0.02))
 
-  # output_bias = tf.get_variable(
-  #     "output_bias", [num_labels], initializer=tf.zeros_initializer())
+  output_bias = tf.get_variable(
+      "output_bias", [num_labels], initializer=tf.zeros_initializer())
 
   with tf.variable_scope("loss"):
     if is_training:
       # I.e., 0.1 dropout
       output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
 
+      logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+      logits = tf.nn.bias_add(logits, output_bias)
+      logits = tf.squeeze(logits, [-1])
+
+
     # new
-    return get_model_outputs(output_layer, labels)
+    return get_model_outputs(logits, labels)
+    # return get_model_outputs(output_layer, labels)
 
-def get_model_outputs(output_layer, labels):
-    dense = tf.keras.layers.Dense(
-        units=1,
-        kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-        bias_initializer=tf.zeros_initializer(),
-    )
-    logits = dense(output_layer)
-    # logits = tf.matmul(output_layer, output_weights, transpose_b=True)
-    # logits = tf.nn.bias_add(logits, output_bias)
-
+def get_model_outputs(logits, labels):
     per_example_loss = tf.square(logits - labels)
     probabilities = logits
-    # probabilities = tf.nn.softmax(logits, axis=-1)
-    # log_probs = tf.nn.log_softmax(logits, axis=-1)
-    #
-    # one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
-    #
-    # per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
     loss = tf.reduce_mean(per_example_loss)
     return (loss, per_example_loss, logits, probabilities)
+
+# def get_model_outputs(output_layer, labels):
+#     dense = tf.keras.layers.Dense(
+#         units=1,
+#         kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+#         bias_initializer=tf.zeros_initializer(),
+#     )
+#     logits = dense(output_layer)
+#     # logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+#     # logits = tf.nn.bias_add(logits, output_bias)
+#
+#     per_example_loss = tf.square(logits - labels)
+#     probabilities = logits
+#     # probabilities = tf.nn.softmax(logits, axis=-1)
+#     # log_probs = tf.nn.log_softmax(logits, axis=-1)
+#     #
+#     # one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+#     #
+#     # per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+#     loss = tf.reduce_mean(per_example_loss)
+#     return (loss, per_example_loss, logits, probabilities)
 
 def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_tpu,
