f23671dc4bfcfeaf6c106ccbfe8ccbcd393114c9
Chia Yew Ken <chiayewken@gmail.com>
2019-09-19 13:02:29 +0800

    new

--- run_squad_modular.py
+++ run_squad_modular.py
@@ -1279,6 +1279,30 @@ def setup_tpu(
   return run_config
 
 
+def run_export_for_serving(estimator, max_seq_length, batch_size, export_dir):
+  # https://medium.com/@joyceye04/deploy-a-servable-bert-qa-model-using-tensorflow-serving-d848f9797d9
+  estimator._export_to_tpu = False  ## !!important to add this
+
+  def serving_input_receiver_fn():
+    feature_spec = {
+        "unique_ids": tf.FixedLenFeature([], tf.int64),
+        "input_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
+        "input_mask": tf.FixedLenFeature([max_seq_length], tf.int64),
+        "segment_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
+    }
+
+    serialized_tf_example = tf.placeholder(dtype=tf.string,
+                                           shape=[batch_size],
+                                           name='input_example_tensor')
+    receiver_tensors = {'examples': serialized_tf_example}
+    features = tf.parse_example(serialized_tf_example, feature_spec)
+    return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+
+  estimator.export_saved_model(
+      export_dir_base=export_dir,
+      serving_input_receiver_fn=serving_input_receiver_fn)
+
+
 def main(
     bert_config_file=None,
     vocab_file=None,
@@ -1380,30 +1404,9 @@ def main(
       train_batch_size=train_batch_size,
       predict_batch_size=predict_batch_size)
 
-  # Serving
-  # https://medium.com/@joyceye04/deploy-a-servable-bert-qa-model-using-tensorflow-serving-d848f9797d9
   if do_export:
-    estimator._export_to_tpu = False  ## !!important to add this
-
-    def serving_input_receiver_fn():
-      feature_spec = {
-          "unique_ids": tf.FixedLenFeature([], tf.int64),
-          "input_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
-          "input_mask": tf.FixedLenFeature([max_seq_length], tf.int64),
-          "segment_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
-      }
-
-      serialized_tf_example = tf.placeholder(dtype=tf.string,
-                                             shape=[predict_batch_size],
-                                             name='input_example_tensor')
-      receiver_tensors = {'examples': serialized_tf_example}
-      features = tf.parse_example(serialized_tf_example, feature_spec)
-      return tf.estimator.export.ServingInputReceiver(features,
-                                                      receiver_tensors)
-
-    estimator.export_saved_model(
-        export_dir_base=export_dir,
-        serving_input_receiver_fn=serving_input_receiver_fn)
+    _batch_size = predict_batch_size
+    run_export_for_serving(estimator, max_seq_length, _batch_size, export_dir)
 
   if do_train:
     model_train(
