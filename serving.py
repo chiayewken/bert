fe4cae0ca1aebbd15da7baea3d92577ee1279108
Chia Yew Ken <chiayewken@gmail.com>
2019-09-19 13:29:42 +0800

    new

--- serving.py
+++ serving.py
@@ -3,26 +3,24 @@ import os
 import fire
 
 
-def remove_adam_vars(ckpt_path: str) -> None:
-  # https://towardsdatascience.com/3-ways-to-optimize-and-export-bert-model-for-online-serving-8f49d774a501
-  #path that contains all 3 ckpt files of your fine-tuned model
-  # path = './bert_output'
-  path = ckpt_path
+def get_checkpoint_paths(output_dir):
+  state = tf.train.get_checkpoint_state(output_dir)
+  paths = state.all_model_checkpoint_paths
+  tf.logging.info("Checkpoint paths: {}".format(paths))
+  return paths
 
-  #path to output the new optimized model
-  output_path = os.path.join(path, 'optimized_model')
+
+def remove_adam_vars(output_dir: str) -> None:
+  # https://towardsdatascience.com/3-ways-to-optimize-and-export-bert-model-for-online-serving-8f49d774a501
+  checkpoint = sorted(get_checkpoint_paths(output_dir))[-1]
+  export_dir = os.path.join(output_dir, 'export')
 
   sess = tf.Session()
-  imported_meta = tf.train.import_meta_graph(
-      os.path.join(path, 'model.ckpt-236962.meta')
-  )  #based on the steps of your fine-tuned model
-  imported_meta.restore(sess, os.path.join(
-      path, 'model.ckpt-236962'))  #based on the steps of your fine-tuned model
+  imported_meta = tf.train.import_meta_graph(".".join([checkpoint, "meta"]))
+  imported_meta.restore(sess, checkpoint)
   my_vars = []
   for var in tf.all_variables():
     if 'adam_v' not in var.name and 'adam_m' not in var.name:
       my_vars.append(var)
   saver = tf.train.Saver(my_vars)
-  saver.save(sess, os.path.join(
-      output_path,
-      'model.ckpt'))  #change model.ckpt to name of your preference
+  saver.save(sess, os.path.join(export_dir, 'model.ckpt'))
